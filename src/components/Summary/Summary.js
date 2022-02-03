9cbcf8f8de59aefa269e1f406055c4a8d04bd5ae
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-03 18:36:56 +0600

    Count Animation

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import {Container, Row, Col, Card} from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+import CountUp from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
 
 class Summary extends Component {
     render() {
@@ -14,12 +16,28 @@ class Summary extends Component {
                                         <Col lg={8} md={6} sm={12} className="countSection">
                                             <Row>
                                                 <Col>
-                                                    <h1 className="countNumber">100</h1>
+                                                    <h1 className="countNumber">
+                                                        <CountUp start={0} end={300}>
+                                                            {({ countUpRef, start }) => (
+                                                                <VisibilitySensor onChange={start} delayedCall>
+                                                                    <span ref={countUpRef}/>
+                                                                </VisibilitySensor>
+                                                            )}
+                                                        </CountUp>
+                                                    </h1>
                                                     <h4 className="countTitle">Total Projects</h4>
                                                 </Col>
                                                 <Col>
-                                                    <h1 className="countNumber">100</h1>
-                                                    <h4 className="countTitle">Total Projects</h4>
+                                                    <h1 className="countNumber">
+                                                        <CountUp start={0} end={650}>
+                                                            {({ countUpRef, start }) => (
+                                                                <VisibilitySensor onChange={start} delayedCall>
+                                                                    <span ref={countUpRef}/>
+                                                                </VisibilitySensor>
+                                                            )}
+                                                        </CountUp>
+                                                    </h1>
+                                                    <h4 className="countTitle">Total Client</h4>
                                                 </Col>
                                             </Row>
                                         </Col>
