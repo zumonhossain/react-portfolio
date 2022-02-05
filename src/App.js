6d078891d464be16804ce35bdd223767b411a406
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-05 16:01:14 +0600

    Home Page Formation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from './logo.svg';
-import TopBanner from "./components/TopBanner/topBanner"
-import Services from "./components/Services/Services"
-import TopNavigation from "./components/TopNavigation/TopNavigation"
-import Analysis from "./components/Analysis/Analysis"
-import Summary from "./components/Summary/Summary"
-import RecentProjects from "./components/RecentProjects/RecentProjects"
-import Courses from "./components/Courses/Courses"
-import Video from "./components/Video/Video"
-import ClientReview from "./components/ClientReview/ClientReview"
-import Footer from "./components/Footer/Footer"
+import React from 'react';
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
-          <TopNavigation/>
-          <TopBanner/>
-          <Services/>
-          <Analysis/>
-          <Summary/>
-          <RecentProjects/>
-          <Courses/>
-          <Video/>
-          <ClientReview/>
-          <Footer/>
+         <HomePage/> 
     </div>
   );
 }
