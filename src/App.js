a8479f3a0af179a602ef203b42e4013bc6c58794
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-05 16:34:34 +0600

    Breadcrumb & Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import HomePage from "./pages/HomePage";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
+import PageTop from "./components/PageTop/PageTop";
 
 function App() {
   return (
     <div>
-         <HomePage/> 
+         <TopNavigation/> 
+         <PageTop pagetitle="About Me" /> 
     </div>
   );
 }
