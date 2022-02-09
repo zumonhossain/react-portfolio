15529df3a16f4b41341c7d30758695780812c7c1
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-09 22:28:41 +0600

    Axios App URL Export

---
+++ src/RestAPI/AppUrl.js
@@ -0,0 +1,24 @@
+class AppUrl{
+
+    static BaseUrl = 'http://127.0.0.1:8000';
+
+    static HomeTopTitle = BaseUrl+'/HomeTopTitle';
+    static TechDesc = BaseUrl+'/TechDesc';
+    static TotalProjectClient = BaseUrl+'/TotalProjectClient';
+    static VideoHome = BaseUrl+'/VideoHome';
+    static ProjectDetails = BaseUrl+'/ProjectDetails';
+    static ProjectAll = BaseUrl+'/ProjectAll';
+    static Project = BaseUrl+'/Project';
+    static Services = BaseUrl+'/Services';
+    static Footer = BaseUrl+'/Footer';
+    static CourseDetails = BaseUrl+'/CourseDetails';
+    static CourseAll = BaseUrl+'/CourseAll';
+    static CourseHome = BaseUrl+'/CourseHome';
+    static ContactSend = BaseUrl+'/ContactSend';
+    static ClientReview = BaseUrl+'/ClientReview';
+    static ChartData = BaseUrl+'/ChartData';
+
+
+}
+
+export default AppUrl;
