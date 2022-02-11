7d0d9bf981fdf1400269c9f18e40ce9b3c5a7845
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-11 09:30:25 +0600

    App Url Modified

--- src/RestAPI/AppUrl.js
+++ src/RestAPI/AppUrl.js
@@ -2,21 +2,21 @@ class AppUrl{
 
     static BaseUrl = 'http://127.0.0.1:8000';
 
-    static HomeTopTitle = BaseUrl+'/HomeTopTitle';
-    static TechDesc = BaseUrl+'/TechDesc';
-    static TotalProjectClient = BaseUrl+'/TotalProjectClient';
-    static VideoHome = BaseUrl+'/VideoHome';
-    static ProjectDetails = BaseUrl+'/ProjectDetails';
-    static ProjectAll = BaseUrl+'/ProjectAll';
-    static Project = BaseUrl+'/Project';
-    static Services = BaseUrl+'/Services';
-    static Footer = BaseUrl+'/Footer';
-    static CourseDetails = BaseUrl+'/CourseDetails';
-    static CourseAll = BaseUrl+'/CourseAll';
-    static CourseHome = BaseUrl+'/CourseHome';
-    static ContactSend = BaseUrl+'/ContactSend';
-    static ClientReview = BaseUrl+'/ClientReview';
-    static ChartData = BaseUrl+'/ChartData';
+    static HomeTopTitle = this.BaseUrl+'/HomeTopTitle';
+    static TechDesc = this.BaseUrl+'/TechDesc';
+    static TotalProjectClient = this.BaseUrl+'/TotalProjectClient';
+    static VideoHome = this.BaseUrl+'/VideoHome';
+    static ProjectDetails = this.BaseUrl+'/ProjectDetails';
+    static ProjectAll = this.BaseUrl+'/ProjectAll';
+    static Project = this.BaseUrl+'/Project';
+    static Services = this.BaseUrl+'/Services';
+    static Footer = this.BaseUrl+'/Footer';
+    static CourseDetails = this.BaseUrl+'/CourseDetails';
+    static CourseAll = this.BaseUrl+'/CourseAll';
+    static CourseHome = this.BaseUrl+'/CourseHome';
+    static ContactSend = this.BaseUrl+'/ContactSend';
+    static ClientReview = this.BaseUrl+'/ClientReview';
+    static ChartData = this.BaseUrl+'/ChartData';
 
 
 }
