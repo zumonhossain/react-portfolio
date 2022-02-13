037e43fc64548ef6fe09a29ff750d3858836f205
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-13 11:44:02 +0600

    Service Page API consuming

--- src/components/AllCourses/AllCourses.js
+++ src/components/AllCourses/AllCourses.js
@@ -4,108 +4,53 @@ import coursesOne from '../../asset/image/courses/courses-1.jpg';
 import coursesTwo from '../../asset/image/courses/courses-2.jpg';
 import {Link} from "react-router-dom";
 
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+
 class AllCourses extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            myData:[]
+        }
+        
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.CourseAll).then(result=>{
+            this.setState({myData:result})
+        })
+    }
+
     render() {
+
+        const myList = this.state.myData
+
+        const myView = myList.map(myList=>{
+
+            return <Col lg={6}  md={12} sm={12} className="p-2">
+                        <Row>
+                            <Col lg={6}  md={6} sm={12}>
+                                <img className="coursesImg" src={myList.small_img} />
+                            </Col>
+                            <Col lg={6}  md={6} sm={12} className="coursesJustufy">
+                                <h5 className="coursesTitle">{myList.short_title}</h5>
+                                <p className="coursesDes">{myList.short_des}</p>
+                                <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
+                            </Col>
+                        </Row>
+                    </Col>
+
+        })
+
         return (
             <Fragment>
                  <Container className="text-center mt-5">
                     <Row>
-                        <Col lg={6}  md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesOne} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">Web Development</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesTwo} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">Web Design</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesTwo} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">React Js</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesOne} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">PHP Laravel</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesTwo} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">React Js</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesOne} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">PHP Laravel</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesTwo} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">React Js</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={6}  md={12} sm={12} className="mt-3 p-2">
-                            <Row>
-                                <Col lg={6}  md={6} sm={12}>
-                                    <img className="coursesImg" src={coursesOne} />
-                                </Col>
-                                <Col lg={6}  md={6} sm={12} className="coursesJustufy">
-                                    <h5 className="coursesTitle">PHP Laravel</h5>
-                                    <p className="coursesDes">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without.</p>
-                                    <a className="coursesLink" href="#"><Link to="/courseDetails" className="allCoursDetailsLink">Details</Link></a>
-                                </Col>
-                            </Row>
-                        </Col>
+
+                       {myView}
+
                     </Row>
                 </Container>
             </Fragment>
