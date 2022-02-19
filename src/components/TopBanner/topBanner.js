50882979d78af525340b5b105f56322101245ce9
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-19 19:56:49 +0600

    Loading Setup For Tob Banner

--- src/components/TopBanner/topBanner.js
+++ src/components/TopBanner/topBanner.js
@@ -3,6 +3,8 @@ import {Container,Row,Col,Button} from "react-bootstrap";
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
 
+import Loading from "../Loading/Loading";
+
 
 class topBanner extends Component {
 
@@ -10,38 +12,49 @@ class topBanner extends Component {
     constructor(){
         super();
         this.state={
-            title:"......",
-            subtitle:"...."
+            title:"",
+            subtitle:"",
+            loading:true
         }
     }
 
 
     componentDidMount() {
         RestClient.GetRequest(AppUrl.HomeTopTitle).then(result=>{
-            this.setState({title:result[0]['home_title'],subtitle:result[0]['home_subtitle']})
+            this.setState({
+                title:result[0]['home_title'],
+                subtitle:result[0]['home_subtitle'],
+                loading:false
+            })
         }).catch(error=>{
             this.setState({title:"?????",subtitle:"?????"})
         })
     }
 
     render() {
-        return (
-            <Fragment>
-                <Container fluid={true} className="topFixedBanner p-0">
-                        <div className="topBannerOverlay">
-                            <Container className="topContent">
-                                    <Row>
-                                        <Col className="text-center">
-                                            <h1 className="topTitle">{this.state.title}</h1>
-                                            <h4 className="topSubTitle">{this.state.subtitle}</h4>
-                                            <Button className="projectButton" variant="primary">More Info</Button>
-                                        </Col>
-                                    </Row>
-                            </Container>
-                        </div>
-                </Container>
-            </Fragment>
-        );
+
+        if(this.state.loading==true){
+            return <Loading/>
+        }
+        else{
+            return (
+                <Fragment>
+                    <Container fluid={true} className="topFixedBanner p-0">
+                            <div className="topBannerOverlay">
+                                <Container className="topContent">
+                                        <Row>
+                                            <Col className="text-center">
+                                                <h1 className="topTitle">{this.state.title}</h1>
+                                                <h4 className="topSubTitle">{this.state.subtitle}</h4>
+                                                <Button className="projectButton" variant="primary">More Info</Button>
+                                            </Col>
+                                        </Row>
+                                </Container>
+                            </div>
+                    </Container>
+                </Fragment>
+            );
+        }
     }
 }
 
