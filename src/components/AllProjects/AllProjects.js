c4e62e71ab821536b31e99783788509ffccdc944
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-19 18:45:18 +0600

    Loading Setup For All Portfolio Page

--- src/components/AllProjects/AllProjects.js
+++ src/components/AllProjects/AllProjects.js
@@ -7,6 +7,7 @@ import {Link} from "react-router-dom";
 
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
+import Loading from "../Loading/Loading";
 
 
 class AllProjects extends Component {
@@ -14,48 +15,57 @@ class AllProjects extends Component {
     constructor(){
         super();
         this.state={
-            myData:[]
+            myData:[],
+            loading:true
         }
         
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppUrl.ProjectAll).then(result=>{
-            this.setState({myData:result})
+            this.setState({
+                myData:result,
+                loading:false
+            })
         })
     }
 
 
     render() {
 
-        const myList = this.state.myData
+        if(this.state.loading==true){
+            return <Loading/>
+        }
+        else{
+            const myList = this.state.myData
 
-        const myView = myList.map(myList=>{
-
-            return <Col lg={4}  md={6} sm={12} className="p-2">
-                        <Card className="projectCard">
-                            <Card.Img className="projectImg" variant="top" src={myList.image_one} />
-                            <Card.Body>
-                                <Card.Title className="projectCardTitle">{myList.project_name}</Card.Title>
-                                <Card.Text className="projectCardDes">{myList.short_description}</Card.Text>
-                                <Button className="projectButton" variant="primary"><Link to={"/ProjectDetails/"+myList.id+"/"+myList.project_name} className="linkStyle">Details</Link></Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-        })
-
-        return (
-            <Fragment>
-                <Container className="text-center mt-5">
-                    <Row>
-
-                        {myView}
-
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+            const myView = myList.map(myList=>{
+    
+                return <Col lg={4}  md={6} sm={12} className="p-2">
+                            <Card className="projectCard">
+                                <Card.Img className="projectImg" variant="top" src={myList.image_one} />
+                                <Card.Body>
+                                    <Card.Title className="projectCardTitle">{myList.project_name}</Card.Title>
+                                    <Card.Text className="projectCardDes">{myList.short_description}</Card.Text>
+                                    <Button className="projectButton" variant="primary"><Link to={"/ProjectDetails/"+myList.id+"/"+myList.project_name} className="linkStyle">Details</Link></Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+    
+            })
+    
+            return (
+                <Fragment>
+                    <Container className="text-center mt-5">
+                        <Row>
+    
+                            {myView}
+    
+                        </Row>
+                    </Container>
+                </Fragment>
+            );
+        }
     }
 }
 
