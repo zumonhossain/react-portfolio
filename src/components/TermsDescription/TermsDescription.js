9c0e84b3fa0bde1b77464048232013ce8a2e4897
zumonhossain <zumon15-13169@diu.edu.bd>
2022-02-19 20:14:06 +0600

    Loading Setup For Terms and Conditions

--- src/components/TermsDescription/TermsDescription.js
+++ src/components/TermsDescription/TermsDescription.js
@@ -5,6 +5,7 @@ import {Container,Row,Col} from "react-bootstrap";
 import RestClient from "../../RestAPI/RestClient";
 import AppUrl from "../../RestAPI/AppUrl";
 import parse from 'html-react-parser';
+import Loading from "../Loading/Loading";
 
 
 class TermsDescription extends Component {
@@ -12,31 +13,40 @@ class TermsDescription extends Component {
     constructor(){
         super();
         this.state={
-            desc:"..."
+            desc:"",
+            loading:true
         }
     }
 
     componentDidMount() {
         RestClient.GetRequest(AppUrl.Information).then(result=>{
-            this.setState({ desc:result[0]['terms'] })
+            this.setState({
+                desc:result[0]['terms'],
+                loading:false
+            })
         })
     }
 
 
     render() {
-        return (
-            <Fragment>
-                <Container className="mt-5">
-                        <Row>
-                            <Col lg={12} md={12} sm={12}>
-                                
-                                { parse(this.state.desc) }
+        if(this.state.loading==true){
+            return <Loading/>
+        }
+        else{
+            return (
+                <Fragment>
+                    <Container className="mt-5">
+                            <Row>
+                                <Col lg={12} md={12} sm={12}>
+                                    
+                                    { parse(this.state.desc) }
 
-                            </Col>
-                        </Row>
-                </Container>
-            </Fragment>
-        );
+                                </Col>
+                            </Row>
+                    </Container>
+                </Fragment>
+            );
+        }
     }
 }
 
